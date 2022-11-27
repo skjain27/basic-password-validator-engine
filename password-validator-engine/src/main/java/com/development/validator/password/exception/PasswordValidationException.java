6fe578dae402cf399c6fb8f5552934d3b90aebc6
Sandeep <sandeep.jain027@gmail.com>
2022-11-27 18:51:26 +0530

    Adding exception class

--- password-validator-engine/src/main/java/com/development/validator/password/exception/PasswordValidationException.java
+++ password-validator-engine/src/main/java/com/development/validator/password/exception/PasswordValidationException.java
@@ -6,6 +6,34 @@ package com.development.validator.password.exception;
  *
  */
 
-public class PasswordValidationException {
+public class PasswordValidationException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	private String message;
+	private Exception exception;
+
+	public PasswordValidationException(String message) {
+		this.setMessage(message);
+	}
+
+	public PasswordValidationException(Exception exception) {
+		this.exception = exception;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Exception getException() {
+		return exception;
+	}
+
+	public void setException(Exception exception) {
+		this.exception = exception;
+	}
 
 }
